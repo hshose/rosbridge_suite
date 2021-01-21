bf855d42746c71f6478bf96fd4bfdf3474e7e93b
Henrik Hose <henrik.s.hose@googlemail.com>
2021-01-21 15:08:22 +0100

    removed publisher

--- rosbridge_library/src/rosbridge_library/capabilities/publish.py
+++ rosbridge_library/src/rosbridge_library/capabilities/publish.py
@@ -57,36 +57,39 @@ class Publish(Capability):
             manager.unregister_timeout = protocol.parameters.get("unregister_timeout")
 
     def publish(self, message):
-        # Do basic type checking
-        self.basic_type_check(message, self.publish_msg_fields)
-        topic = message["topic"]
-        latch = message.get("latch", False)
-        queue_size = message.get("queue_size", 100)
 
-        if Publish.topics_glob is not None and Publish.topics_glob:
-            self.protocol.log("debug", "Topic security glob enabled, checking topic: " + topic)
-            match = False
-            for glob in Publish.topics_glob:
-                if (fnmatch.fnmatch(topic, glob)):
-                    self.protocol.log("debug", "Found match with glob " + glob + ", continuing publish...")
-                    match = True
-                    break
-            if not match:
-                self.protocol.log("warn", "No match found for topic, cancelling publish to: " + topic)
-                return
-        else:
-            self.protocol.log("debug", "No topic security glob, not checking publish.")
+        self.protocol.log("warn", "You tried to publish to a topic: " + topic + " This is not allowed!")
+        return
+        # # Do basic type checking
+        # self.basic_type_check(message, self.publish_msg_fields)
+        # topic = message["topic"]
+        # latch = message.get("latch", False)
+        # queue_size = message.get("queue_size", 100)
 
-        # Register as a publishing client, propagating any exceptions
-        client_id = self.protocol.client_id
-        manager.register(client_id, topic, latch=latch, queue_size=queue_size)
-        self._published[topic] = True
+        # if Publish.topics_glob is not None and Publish.topics_glob:
+        #     self.protocol.log("debug", "Topic security glob enabled, checking topic: " + topic)
+        #     match = False
+        #     for glob in Publish.topics_glob:
+        #         if (fnmatch.fnmatch(topic, glob)):
+        #             self.protocol.log("debug", "Found match with glob " + glob + ", continuing publish...")
+        #             match = True
+        #             break
+        #     if not match:
+        #         self.protocol.log("warn", "No match found for topic, cancelling publish to: " + topic)
+        #         return
+        # else:
+        #     self.protocol.log("debug", "No topic security glob, not checking publish.")
 
-        # Get the message if one was provided
-        msg = message.get("msg", {})
+        # # Register as a publishing client, propagating any exceptions
+        # client_id = self.protocol.client_id
+        # manager.register(client_id, topic, latch=latch, queue_size=queue_size)
+        # self._published[topic] = True
 
-        # Publish the message
-        manager.publish(client_id, topic, msg, latch=latch, queue_size=queue_size)
+        # # Get the message if one was provided
+        # msg = message.get("msg", {})
+
+        # # Publish the message
+        # manager.publish(client_id, topic, msg, latch=latch, queue_size=queue_size)
 
     def finish(self):
         client_id = self.protocol.client_id
